baaea3da57c3fe8acd6472975693ea64041804bb
warda-mohtaj <115171252+warda-mohtaj@users.noreply.github.com>
2023-04-05 10:23:20 +0200

    Create job2.sql

---
+++ Jour2/job2.sql
@@ -0,0 +1,13 @@
+USE LaPlateforme
+
+CREATE TABLE etage (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nom VARCHAR(255) NOT NULL,
+numero INT,
+superficie INT);
+
+CREATE TABLE salles (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nom VARCHAR(255) NOT NULL,
+id_etage INT,
+capacite INT);
